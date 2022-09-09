cbc557ca5ce821d5bdf52d87d9334cdfd7567dfe
l92169 <71372570+l92169@users.noreply.github.com>
2022-09-09 17:08:06 +0300

    Initial commit

---
+++ yandex_backend_school_2022_java/src/main/java/ru/yandex/yandex_backend_2022_java/YandexBackendSchool2022JavaApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.yandex_backend_2022_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexBackendSchool2022JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexBackendSchool2022JavaApplication.class, args);
+	}
+
+}
---
+++ yandex_backend_school_2022_java/src/test/java/ru/yandex/yandex_backend_2022_java/YandexBackendSchool2022JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.yandex_backend_2022_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexBackendSchool2022JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
